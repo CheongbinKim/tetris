6f6d22998691904372af288f781454da78815141
iannohgithub <iannoh@gmail.com>
2018-06-03 20:03:47 +0900

    18060
    180603

--- KLib-master/KMain.cpp
+++ KLib-master/KMain.cpp
@@ -11,7 +11,7 @@ void OnDraw(HDC hdc);
 void OnLButtonDown(HWND hWnd, LPARAM lParam);
 void OnKeyDown(HWND hWnd, WPARAM wParam);
 void OnTimer(WPARAM wParam);
-int Check(int bs, int rot, int sx);
+int Check(int rotation, int sx, int sy);
 
 
 #define RECT_WIDTH 15
@@ -35,170 +35,172 @@ int Check(int bs, int rot, int sx);
 
 HWND mHwnd;
 HINSTANCE mInst;
-int r = 1;
-int block_shape;
-int block[7][4][4][4]= {
-	{ 
+int rot = 1;
+int block_shape = block_Z;
+int block[7][4][4][4] = {
 		{
-		0,0,0,0,
-		0,1,1,0,
-		0,1,1,0,
-		0,0,0,0 },
-		{ 
-		0,0,0,0,
-		0,1,1,0,
-		0,1,1,0,
-		0,0,0,0 },
-		{ 
-		0,0,0,0,
-		0,1,1,0,
-		0,1,1,0,
-		0,0,0,0 },
+			{
+				0,0,0,0,
+				0,1,1,0,
+				0,1,1,0,
+				0,0,0,0 },
+				{
+					0,0,0,0,
+					0,1,1,0,
+					0,1,1,0,
+					0,0,0,0 },
+					{
+						0,0,0,0,
+						0,1,1,0,
+						0,1,1,0,
+						0,0,0,0 },
+						{
+							0,0,0,0,
+							0,1,1,0,
+							0,1,1,0,
+							0,0,0,0 }
+		},
 		{
-		0,0,0,0,
-		0,1,1,0,
-		0,1,1,0,
-		0,0,0,0 } 
-	},
-	{ 
+			{
+				0,0,0,0,
+				1,1,1,1,
+				0,0,0,0,
+				0,0,0,0 },
+				{
+					0,0,1,0,
+					0,0,1,0,
+					0,0,1,0,
+					0,0,1,0 },
+					{
+						0,0,0,0,
+						1,1,1,1,
+						0,0,0,0,
+						0,0,0,0 },
+						{
+							0,0,1,0,
+							0,0,1,0,
+							0,0,1,0,
+							0,0,1,0 }
+		},
 		{
-		0,0,0,0,
-		0,0,0,0,
-		1,1,1,1,
-		0,0,0,0 },
+			{
+				0,0,0,0,
+				0,1,1,0,
+				0,0,1,1,
+				0,0,0,0 },
+				{
+					0,0,0,1,
+					0,0,1,1,
+					0,0,1,0,
+					0,0,0,0 },
+					{
+						0,0,0,0,
+						0,1,1,0,
+						0,0,1,1,
+						0,0,0,0 },
+						{
+							0,0,0,1,
+							0,0,1,1,
+							0,0,1,0,
+							0,0,0,0 }
+		},
 		{
-		0,1,0,0,
-		0,1,0,0,
-		0,1,0,0,
-		0,1,0,0 },
-		{ 
-		0,0,0,0,
-		0,0,0,0,
-		1,1,1,1,
-		0,0,0,0 },
+			{
+				0,0,0,0,
+				0,0,1,1,
+				0,1,1,0,
+				0,0,0,0 },
+				{
+					0,0,1,0,
+					0,0,1,1,
+					0,0,0,1,
+					0,0,0,0 },
+					{
+						0,0,0,0,
+						0,0,1,1,
+						0,1,1,0,
+						0,0,0,0 },
+						{
+							0,0,1,0,
+							0,0,1,1,
+							0,0,0,1,
+							0,0,0,0 }
+		},
 		{
-		0,1,0,0,
-		0,1,0,0,
-		0,1,0,0,
-		0,1,0,0 } 
-	},
-	{ 
-		{ 
-		0,0,0,0,
-		1,1,0,0,
-		0,1,1,0,
-		0,0,0,0 },
+			{
+				0,0,0,0,
+				0,1,1,1,
+				0,1,0,0,
+				0,0,0,0 },
+				{
+					0,0,1,0,
+					0,0,1,0,
+					0,0,1,1,
+					0,0,0,0 },
+					{
+						0,0,0,1,
+						0,1,1,1,
+						0,0,0,0,
+						0,0,0,0 },
+						{
+							0,1,1,0,
+							0,0,1,0,
+							0,0,1,0,
+							0,0,0,0 }
+		},
 		{
-		0,0,0,0,
-		0,0,1,0,
-		0,1,1,0,
-		0,1,0,0 },
-		{ 
-		0,0,0,0,
-		1,1,0,0,
-		0,1,1,0,
-		0,0,0,0 },
-		{ 
-		0,0,0,0,
-		0,0,1,0,
-		0,1,1,0,
-		0,1,0,0 } 
-	},
-	{ 
-		{ 
-		0,0,0,0,
-		0,1,1,0,
-		1,1,0,0,
-		0,0,0,0 },
-		{ 
-		0,0,0,0,
-		1,0,0,0,
-		1,1,0,0,
-		0,1,0,0 },
-		{ 
-		0,0,0,0,
-		0,1,1,0,
-		1,1,0,0,
-		0,0,0,0 },
-		{ 
-		0,0,0,0,
-		1,0,0,0,
-		1,1,0,0,
-		0,1,0,0 } 
-	},
-	{ 
-		{ 
-		0,0,0,0,
-		0,0,1,0,
-		1,1,1,0,
-		0,0,0,0 },
-		{ 
-		0,0,0,0,
-		1,1,0,0,
-		0,1,0,0,
-		0,1,0,0 },
-		{ 
-		0,0,0,0,
-		0,0,0,0,
-		1,1,1,0,
-		1,0,0,0 },
-		{ 
-		0,0,0,0,
-		1,0,0,0,
-		1,0,0,0,
-		1,1,0,0 } 
-	},
-	{ 
-		{ 
-		0,0,0,0,
-		1,0,0,0,
-		1,1,1,0,
-		0,0,0,0 },
-		{ 
-		0,0,0,0,
-		0,0,1,0,
-		0,0,1,0,
-		0,1,1,0 },
-		{ 
-		0,0,0,0,
-		1,1,1,0,
-		0,0,1,0,
-		0,0,0,0 },
-		{ 
-		0,0,0,0,
-		0,1,1,0,
-		0,1,0,0,
-		0,1,0,0 } 
-	},
-	{ 
+			{
+				0,0,0,0,
+				0,1,1,1,
+				0,0,0,1,
+				0,0,0,0 },
+				{
+					0,0,1,1,
+					0,0,1,0,
+					0,0,1,0,
+					0,0,0,0 },
+					{
+						0,1,0,0,
+						0,1,1,1,
+						0,0,0,0,
+						0,0,0,0 },
+						{
+							0,0,1,0,
+							0,0,1,0,
+							0,1,1,0,
+							0,0,0,0 }
+		},
 		{
-		0,0,0,0,
-		0,0,0,0,
-		0,1,0,0,
-		1,1,1,0},
-		{ 
-		0,0,0,0,
-		0,0,1,0,
-		0,1,1,0,
-		0,0,1,0 },
-		{ 
-		0,0,0,0,
-		1,1,1,0,
-		0,1,0,0,
-		0,0,0,0 },
-		{ 
-		0,0,0,0,
-		1,0,0,0,
-		1,1,0,0,
-		1,0,0,0} 
-	}
+			{
+				0,0,0,0,
+				0,1,1,1,
+				0,0,1,0,
+				0,0,0,0 },
+				{
+					0,0,1,0,
+					0,0,1,1,
+					0,0,1,0,
+					0,0,0,0 },
+					{
+						0,0,1,0,
+						0,1,1,1,
+						0,0,0,0,
+						0,0,0,0 },
+						{
+							0,0,1,0,
+							0,1,1,0,
+							0,0,1,0,
+							0,0,0,0 }
+		}
 };
 
+int map[SIZE_WIDTH + 2][SIZE_HEIGHT + 2] = { 0 };
 
 HBITMAP hBitmap;
 RECT crt;
 
-int gx=(SIZE_WIDTH/2)-2, gy;
+int gx=(SIZE_WIDTH/2)-2, gy=1;
+
 
 
 void OnCreate(HWND hWnd, HINSTANCE hInst)
@@ -208,6 +210,21 @@ void OnCreate(HWND hWnd, HINSTANCE hInst)
 	srand(time(NULL));
 
 	SetTimer(hWnd, 1, 1000, NULL);
+
+	for (int y = 0; y < SIZE_HEIGHT + 2; y++)
+	{
+		for (int x = 0; x < SIZE_WIDTH+2; x++)
+		{
+			if ((x*y == 0) ||( x == SIZE_WIDTH + 1 ))
+			{
+				map[y][x] = 1;
+			}
+			else if (y == SIZE_HEIGHT + 1)
+			{
+				map[y][x] = 2;
+			}
+		}
+	}
 }
 
 
@@ -223,13 +240,24 @@ void OnDraw(HDC hdc)
 
 	for (int y = 0; y < SIZE_HEIGHT+2; y++)
 	{
-		for (int x = 0; x < SIZE_WIDTH+2; x++)
+		for (int x = 0; x < SIZE_WIDTH + 2; x++)
 		{
 			//Rectangle(hdc, RECT_WIDTH*x, RECT_HEIGHT*y, RECT_WIDTH*x + RECT_WIDTH, RECT_HEIGHT*y + RECT_HEIGHT);
-			if (x*y == 0 || x == SIZE_WIDTH + 1 || y == SIZE_HEIGHT + 1)
+			if ((map[y][x] == 1))
 			{
 				HBRUSH hBrush, oBrush;
 				hBrush = CreateSolidBrush(RGB(0, 0, 0));
+				oBrush = (HBRUSH)SelectObject(hdc, hBrush);
+				Rectangle(hdc, RECT_WIDTH * x, RECT_HEIGHT *y, RECT_WIDTH*(x + 1), RECT_HEIGHT * (y + 1));
+
+
+				SelectObject(hdc, oBrush);
+				DeleteObject(hBrush);
+			}
+			else if ((map[y][x] == 2))
+			{
+				HBRUSH hBrush, oBrush;
+				hBrush = CreateSolidBrush(RGB(255, 0, 0));
 				oBrush = (HBRUSH)SelectObject(hdc, hBrush);
 				Rectangle(hdc, RECT_WIDTH * x, RECT_HEIGHT *y, RECT_WIDTH*(x + 1), RECT_HEIGHT * (y + 1));
 
@@ -245,13 +273,13 @@ void OnDraw(HDC hdc)
 		for (int x = 0; x < 4; x++)
 		{
 			//Rectangle(hdc, RECT_WIDTH * x, RECT_HEIGHT * y, RECT_WIDTH * x + RECT_WIDTH, RECT_HEIGHT * y + RECT_HEIGHT);
-			if (block[block_T][r][y][x] ==1)
+			if (block[block_shape][rot][y][x] ==1)
 			{
 				HBRUSH hBrush, oBrush;
 				hBrush = CreateSolidBrush(RGB(255, 255, 0));
 				oBrush = (HBRUSH)SelectObject(hdc, hBrush);
 
-				Rectangle(hdc, RECT_WIDTH * (x + gx + 1), RECT_HEIGHT *(y + gy + 1), RECT_WIDTH*(x + gx + 1 + 1), RECT_HEIGHT * (y + gy + 1 + 1));
+				Rectangle(hdc, RECT_WIDTH * (x + gx), RECT_HEIGHT *(y + gy), RECT_WIDTH*(x + gx + 1), RECT_HEIGHT * (y + gy + 1));
 
 				SelectObject(hdc, oBrush);
 				DeleteObject(hBrush);
@@ -267,6 +295,8 @@ void OnDraw(HDC hdc)
 
 }
 
+
+
 void OnLButtonDown(HWND hWnd, LPARAM lParam)
 {
 	
@@ -275,48 +305,101 @@ void OnLButtonDown(HWND hWnd, LPARAM lParam)
 }
 
 
+
 void OnKeyDown(HWND hWnd,WPARAM wParam)
 {
 	switch (wParam)
 	{
 	case VK_LEFT:
-		if (Check(block_shape, r, gx-1)==0)
+		if (Check(rot, gx-1, gy)==0)
 		{
 			gx--;
 		}
 		break;
 	case VK_RIGHT:
-		if (Check(block_shape, r, gx+1)==-0)
+		if (Check(rot, gx+1, gy)==0)
 		{
 			gx++;
 		}
 		break;
 	case VK_DOWN:
-		gy ++;
-		gy ++;
+		if (Check(rot, gx, gy + 1) == 0)
+		{
+			gy += 2;
+		}
 		break;
 	case VK_UP:
-		r=(r+1)%4;
+		if (Check(((rot + 1) % 4), gx, gy)==0)
+		{
+			rot = (rot + 1) % 4;
+		}
 		break;
 	}
 	InvalidateRect(hWnd, NULL, true);
 }
+
+
 
 void OnTimer(WPARAM wParam)
 {
 	switch (wParam)
 	{
 	case 1:
-		gy ++;
+		if (Check(rot, gx, gy + 1) == 0)
+		{
+			gy++;
+		}
+		else if (Check(rot, gx, gy + 1) == -2)
+		{
+			for (int y = 0; y < 4; y++)
+			{
+				for (int x = 0; x < 4; x++)
+				{
+					if (block[block_shape][rot][y][x] == 1)
+					{
+						map[gy + y][gx + x] = 2;
+					}
+				}
+			}
+			block_shape = rand() % 7;
+			gx = (SIZE_WIDTH / 2) - 2;
+			gy = 1;
+
+			// ÁÙÃ¼Å©
+			int check = 0;
+			for (int y = 0; y < SIZE_HEIGHT + 2; y++)
+			{
+				for (int x = 0; x < SIZE_WIDTH + 2; x++)
+				{
+					if (map[y][x] > 0)
+					{
+						check++;
+					}
+					if (check == SIZE_WIDTH + 2)
+					{
+						for (int check_y = y; check_y > 0; y--)
+						{
+							for (int x = 1; x < SIZE_WIDTH + 1; x++)
+							{
+								map[y + 1][x] = map[y][x];
+							}
+						}
+					}
+					check = 0;
+				}
+			}
+		}
 		break;
 	}
 	InvalidateRect(mHwnd, NULL, true);
 
 }
 
-int Check(int bs, int rot, int sx)
+
+
+int Check(int rotation, int sx, int sy)
 {
-	for (int y = 0; y < 4; y++)
+	/*for (int y = 0; y < 4; y++)
 	{
 		for (int x = 0; x < 4; x++)
 		{
@@ -325,6 +408,24 @@ int Check(int bs, int rot, int sx)
 				if ((sx + x == 0) || (sx + x == SIZE_WIDTH))
 				{
 					return -1;
+				}
+			}
+		}
+	}
+	return 0;*/
+	for (int y = 0; y < 4; y++)
+	{
+		for (int x = 0; x < 4; x++)
+		{
+			if (block[block_shape][rotation][y][x] == 1)
+			{
+				if (map[sy + y][sx + x] == 1)
+				{
+					return -1;
+				}
+				else if (map[sy + y][sx + x] == 2)
+				{
+					return -2;
 				}
 			}
 		}
